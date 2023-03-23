ad3e950e5afaba9f0f1aad04f53d061f671443cf
Tiago Faria Gouvêa <112782437+TiagoGouvea2000@users.noreply.github.com>
2023-03-23 09:26:26 -0300

    Message

---
+++ src/componentes/Comment.tsx
@@ -0,0 +1,52 @@
+import styles from './Comment.module.css'
+
+import { ThumbsUp } from 'phosphor-react'
+import { Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
+import { useState } from 'react'
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({content,onDeleteComment}: CommentProps ) {
+    const [likeCount , setLikeCount] = useState(0);
+    
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        })
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/TiagoGouvea2000.png" alt='' />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentBoxContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Tiago Faira Gouvea</strong>
+                            <time title='09 de Dezembro às 16:20h' dateTime='2022-12-09 16:20:00'>Cerca de 2h</time>     
+                        </div>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24}/>    
+                        </button>    
+                    </header>                
+                    <p>{content}</p>
+                </div> 
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
